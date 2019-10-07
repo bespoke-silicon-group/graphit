db2c2b615a8f814280dfb2abab173f7578ec150a
eafurst <eafurst@cs.washington.edu>
2019-10-07 13:39:36 -0700

    debugged bfs templated host code

--- test/hammerblade-rt/bfs_templated.cpp
+++ test/hammerblade-rt/bfs_templated.cpp
@@ -14,20 +14,24 @@ GlobalScalar<hb_mc_eva_t> parent_dev;
 
 int main(int argc, char * argv[]){
   hammerblade::builtin_loadMicroCodeFromFile(ucode_path);
-  parent_dev = GlobalScalar<hb_mc_eva_t>("parent");
+  //parent_dev = GlobalScalar<hb_mc_eva_t>("parent");
   edges = hammerblade::builtin_loadEdgesFromFileToHB ( argv[(1) ]) ;
   Device::Ptr device = Device::GetInstance();
 
   device->enqueueJob("parent_generated_vector_op_apply_func_0_kernel",{edges.num_nodes(), edges.num_edges(),edges.num_nodes()});
   device->runJobs();
 
-  Vector<int32_t> frontier = new Vector<int32_t>(hammerblade::builtin_getVerticesHB(edges), 0);
-  Vector<int32_t> next_frontier = new Vector<int32_t>(hammerblade::builtin_getVerticesHB(edges), 0);
+  Vector<int32_t> frontier(hammerblade::builtin_getVerticesHB(edges), 0);
+  Vector<int32_t> next_frontier(hammerblade::builtin_getVerticesHB(edges), 0);
+  Vector<int32_t> parent(hammerblade::builtin_getVerticesHB(edges), -1);
 
-  frontier.insert(0, 1);
+  frontier.insert(8, 1);
 
-  parent.insert(0, 0);
-  while ( (hammerblade::builtin_getVertexSetSizeHB(frontier, edges.num_edges()) ) != ((0) ))
+  parent.insert(8, 8);
+
+  int host_frontier[edges.num_nodes()];
+
+  while ( (hammerblade::builtin_getVertexSetSizeHB(frontier, edges.num_nodes()) ) != ((0) ))
   {
       device->enqueueJob("edgeset_apply_push_serial_from_vertexset_to_filter_func_with_frontier_call",
                         {edges.getOutIndicesAddr(),
@@ -40,11 +44,14 @@ int main(int argc, char * argv[]){
                          edges.num_nodes()});
       device->runJobs();
 
-      frontier = next_frontier;
+      //frontier = next_frontier;
+      next_frontier.copyToHost(host_frontier, edges.num_nodes());
+      frontier.copyToDevice(host_frontier, edges.num_nodes());
       next_frontier.assign(0, edges.num_nodes(), 0);
   }
-  int * host_parent;
-  hammerblade::read_global_buffer(host_parent, parent_dev, edges.num_edges());
+  int host_parent[edges.num_nodes()];
+  //hammerblade::read_global_buffer(host_parent, parent_dev, edges.num_edges());
+  parent.copyToHost(host_parent, edges.num_nodes());
   std::cerr << "Results of BFS" << std::endl;
   for(auto i : host_parent) {
       std::cerr << i << std::endl;

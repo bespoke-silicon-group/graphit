74a4aeef0d7a356a41ffc9b664ad832a447919d7
eafurst <eafurst@cs.washington.edu>
2019-10-11 10:55:43 -0400

    fixing init bug by changing to vector *

--- src/runtime_lib/hammerblade/host/parallel_vector.hpp
+++ src/runtime_lib/hammerblade/host/parallel_vector.hpp
@@ -25,19 +25,19 @@ public:
   ParVector () :
           _blocks(1),
           _length(0) {
-          _vec();
+          _vec = new Vector<T>();
   }
 
   ParVector (size_t length, size_t blocks) :
           _blocks(blocks),
           _length(length) {
-            _vec(length * blocks);
+            _vec = new Vector<T>(length * blocks);
           }
 
   ParVector (size_t length, size_t blocks, T val) :
           _length(length),
           _blocks(blocks) {
-            _vec(length * blocks, val);
+          _vec = new Vector<T>((length * blocks), val);
           }
 
   ParVector (ParVector &&other) {
@@ -53,8 +53,8 @@ public:
 
   ~ParVector() { exit(); }
 
-  hb_mc_eva_t getAddr()   const { return _vec.getBase(); }
-  hb_mc_eva_t getBase()   const { return _vec.getBase(); }
+  hb_mc_eva_t getAddr()   const { return _vec->getBase(); }
+  hb_mc_eva_t getBase()   const { return _vec->getBase(); }
 
   hb_mc_eva_t getLength() const { return _length; }
 
@@ -64,33 +64,33 @@ public:
           if (end > getLength())
                   throw ParVector::out_of_bounds(end, getLength());
 
-          _vec.assign(start, end, val);
+          _vec->assign(start, end, val);
   }
 
   T at(size_t pos) const {
           if (pos >= _length)
                   throw ParVector::out_of_bounds(pos, _length);
 
-          return(_vec.at(pos));
+          return(_vec->at(pos));
   }
 
   void insert(size_t pos, const T & val) {
           if(pos >= _length)
                   throw ParVector::out_of_bounds(pos, _length);
           //NOTE(Emily): do we want to insert for every thread's replication?
-          _vec.insert(pos, val);
+          _vec->insert(pos, val);
   }
 
   void copyToHost(T * host, size_t n) const {
           if(n > (_length * _blocks))
                   throw ParVector::out_of_bounds(n, (_length * _blocks));
-          _vec.copyToHost(host, n);
+          _vec->copyToHost(host, n);
   }
 
   void copyToDevice(const T * host, size_t n) {
           if(n > (_length * _blocks))
                   throw ParVector::out_of_bounds(n, (_length * _blocks));
-          _vec.copyToDevice(host, n);
+          _vec->copyToDevice(host, n);
   }
 
 private:
@@ -98,25 +98,25 @@ private:
   void swap(const ParVector &other) {
     std::swap(other._blocks, _blocks);
     std::swap(other._length, _length);
-    _vec.swap(other._vec);
+    _vec->swap(other._vec);
   }
 
   void moveFrom(ParVector &other) {
     _blocks = other._blocks;
     _length = other._length;
-    _vec.moveFrom(other._vec);
+    _vec->moveFrom(other._vec);
   }
 
   void init(void) {
-    _vec.init();
+    _vec->init();
   }
 
   void exit(void) {
-    _vec.exit();
+    _vec->exit();
   }
 
   size_t      _blocks; //!< the number of threads accessing this vec
   size_t      _length; //!< the number of elements in the array (not * _blocks yet)
-  Vector<T>   _vec; //!< the actual Vector item storing the data
+  Vector<T> *  _vec; //!< the actual Vector item storing the data
 };
 };

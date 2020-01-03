a98b9798e0102dff16bcce39cd0a34a555286cc8
eafurst <eafurst@cs.washington.edu>
2020-01-02 19:10:42 -0800

    initial outline for atomic operations on device

---
+++ src/runtime_lib/hammerblade/device/atomics.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <bsg_manycore.h>
+#include <bsg_set_tile_x_y.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+//method to atomically update a value in global mem
+static inline void atomic_update()
+{
+
+}
+#ifdef __cplusplus
+}
+#endif

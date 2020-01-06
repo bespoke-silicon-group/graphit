61aa0a7ea1ad6497fcd5e4fc20cfe223e43a35cb
eafurst <eafurst@cs.washington.edu>
2020-01-05 16:11:43 -0800

    intial basic lock aquire/update val/lock release method

--- src/runtime_lib/hammerblade/device/atomics.h
+++ src/runtime_lib/hammerblade/device/atomics.h
@@ -6,9 +6,11 @@
 extern "C" {
 #endif
 //method to atomically update a value in global mem
-static inline void atomic_update()
+static inline void atomic_update_val(int * ptr, int pos, int val, int * lock)
 {
-
+  int result = bsg_amoswap_aq(lock, bsg_id); //set lock to bsg_id
+  ptr[pos] = val;
+  result = bsg_amoswap_rl(lock, bsg_id);
 }
 #ifdef __cplusplus
 }

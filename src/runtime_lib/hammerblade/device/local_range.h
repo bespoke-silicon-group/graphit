2335b5b7d514011f309046b42c3f27e2ff797186
Emily Furst <eafurst@cs.washington.edu>
2020-04-09 10:54:26 -0700

    more of a template of the edge aware local range

--- src/runtime_lib/hammerblade/device/local_range.h
+++ src/runtime_lib/hammerblade/device/local_range.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <bsg_manycore.h>
 #include <bsg_set_tile_x_y.h>
+#include <bsg_tile_group_barrier.h>
+INIT_TILE_GROUP_BARRIER(r_barrier, c_barrier, 0, bsg_tiles_X-1, 0, bsg_tiles_Y-1);
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -15,16 +18,29 @@ static inline void local_range(int n, int *start, int *end)
 }
 #endif
 
+void recursive_range(int n, int e, int grain size, int * start_idx, int * end_idx, int * edge_index)
+{
+  //TODO(thread idx = 0 does the work to assign all the ranges)
+
+
+}
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-static inline void edge_aware_local_range(int n, int e, int grain_size, int *start, int *end, int *edge_index)
+static inline void edge_aware_local_range(int n, int e, int *start, int *end, int *edge_index)
 {
         //TODO(Emily): need to implement this function to distribute work based on grain size
-        int sz = n / (bsg_tiles_X * bsg_tiles_Y);
-        *start = bsg_id * sz;
-        *end = *start + sz;
-        *end = *end < n ? *end : n;
+        int grain_size = e / (bsg_tiles_X * bsg_tiles_Y);
+        int start_idx[bsg_tiles_X * bsg_tiles_Y] = {0};
+        int end_idx[bsg_tiles_X * bsg_tiles_Y] = {0};
+        if(bsg_id == 0) {
+          recursive_range(n, e, grain_size, start_idx, end_idx, edge_index);
+        }
+        //barrier here?
+        bsg_tile_group_barrier(&r_barrier, &c_barrier);
+        *start = start_idx[bsg_id];
+        *end = end_idx[bsg_id];
 }
 #ifdef __cplusplus
 }

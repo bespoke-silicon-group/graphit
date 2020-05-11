96f3486ed648ab1c818d00c37452a34b44d901bf
Emily Furst <eafurst@cs.washington.edu>
2020-05-11 08:36:08 -0700

    updates on timing files

--- test/hammerblade-rt/bfs_push_time.cpp
+++ test/hammerblade-rt/bfs_push_time.cpp
@@ -14,27 +14,52 @@ int  * __restrict parent;
 template <typename TO_FUNC , typename APPLY_FUNC> VertexSubset<NodeID>* edgeset_apply_pull_serial_from_vertexset_to_filter_func_with_frontier(Graph & g , VertexSubset<NodeID>* from_vertexset, TO_FUNC to_func, APPLY_FUNC apply_func)
 {
   int64_t numVertices = g.num_nodes(), numEdges = g.num_edges();
+  from_vertexset->toSparse();
+  long m = from_vertexset->size();
+  // used to generate nonzero indices to get degrees
+  uintT *degrees = newA(uintT, m);
+  // We probably need this when we get something that doesn't have a dense set, not sure
+  // We can also write our own, the eixsting one doesn't quite work for bitvectors
+  //from_vertexset->toSparse();
+  {
+      parallel_for (long i = 0; i < m; i++) {
+          NodeID v = from_vertexset->dense_vertex_set_[i];
+          degrees[i] = g.out_degree(v);
+      }
+  }
+  uintT outDegrees = sequence::plusReduce(degrees, m);
   VertexSubset<NodeID> *next_frontier = new VertexSubset<NodeID>(g.num_nodes(), 0);
-  bool * next = newA(bool, g.num_nodes());
-  parallel_for (int i = 0; i < numVertices; i++)next[i] = 0;
-  from_vertexset->toDense();
+  if (numVertices != from_vertexset->getVerticesRange()) {
+      cout << "edgeMap: Sizes Don't match" << endl;
+      abort();
+  }
+  if (outDegrees == 0) return next_frontier;
+  uintT *offsets = degrees;
+  long outEdgeCount = sequence::plusScan(offsets, degrees, m);
+  uintE *outEdges = newA(uintE, outEdgeCount);
   int traversed = 0;
-  for ( NodeID d=0; d < g.num_nodes(); d++) {
-    if (to_func(d)){
-      for(NodeID s : g.in_neigh(d)){
-        traversed++;
-        if (from_vertexset->bool_map_[s] ) {
-          if( apply_func ( s , d ) ) {
-            next[d] = 1;
-            if (!to_func(d)) break;
-          }
-        }
-      } //end of loop on in neighbors
-    } //end of to filtering
-  } //end of outer for loop
-  next_frontier->num_vertices_ = sequence::sum(next, numVertices);
-  next_frontier->bool_map_ = next;
-  next_frontier->is_dense = true;
+  for (long i=0; i < m; i++) {
+    NodeID s = from_vertexset->dense_vertex_set_[i];
+    int j = 0;
+    uintT offset = offsets[i];
+    for(NodeID d : g.out_neigh(s)){
+      traversed++;
+      if (to_func(d)) {
+        if( apply_func ( s , d  ) ) {
+          outEdges[offset + j] = d;
+        } else { outEdges[offset + j] = UINT_E_MAX; }
+      } //end of to func
+       else { outEdges[offset + j] = UINT_E_MAX;  }
+      j++;
+    } //end of for loop on neighbors
+  }
+  //std::cout << "traversed edges for this iteration: " << traversed << std::endl;
+  uintE *nextIndices = newA(uintE, outEdgeCount);
+  long nextM = sequence::filter(outEdges, nextIndices, outEdgeCount, nonMaxF());
+  free(outEdges);
+  free(degrees);
+  next_frontier->num_vertices_ = nextM;
+  next_frontier->dense_vertex_set_ = nextIndices;
   return next_frontier;
 } //end of edgeset apply function
 struct parent_generated_vector_op_apply_func_0
@@ -96,6 +121,7 @@ int main(int argc, char * argv[])
   parent[root ] = (root) ;
   std::cout << "root: " << root << std::endl;
   int iter_c = 0;
+  bool written = false;
   while ( (builtin_getVertexSetSize(frontier) ) != ((0) ))
   {
     VertexSubset<int> *  output ;
@@ -108,11 +134,8 @@ int main(int argc, char * argv[])
     frontier = output;
     iter_c++;
     //double percentage = builtin_getVertexSetSize(frontier) / ((double) builtin_getVertices(edges));
-    std::cout << "size of input frontier/total nodes: " << percentage << std::endl;
+    std::cout << "size of frontier/total nodes: " << percentage << std::endl;
     std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n" << std::endl;
-    // if(builtin_getVertexSetSize(frontier) > (builtin_getVertices(edges)/10)){
-    //   break;
-    // }
   }
   deleteObject(frontier) ;
 };
